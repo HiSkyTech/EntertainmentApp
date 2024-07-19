82250ad845185172e19a1be14b6622312c7b59a2
HiSkyTech <hiskytechs@gmail.com>
2024-07-19 11:36:15 +0500

    initial

---
+++ app/src/main/java/com/urdubolo/com/pk/Ui/ActivityEditProfile.kt
@@ -0,0 +1,21 @@
+package com.urdubolo.com.pk.Ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.urdubolo.com.pk.R
+
+class ActivityEditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/urdubolo/com/pk/Ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.urdubolo.com.pk.Ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.urdubolo.com.pk.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
